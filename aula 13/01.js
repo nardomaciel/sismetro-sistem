99cb5868ff8429b547905ad826dfe9c02664a0c1
nardo <129193276+nardomaciel@users.noreply.github.com>
2023-09-19 15:58:21 -0300

    calculadora

---
+++ aula 13/01.js
@@ -0,0 +1,18 @@
+function soma(n1,n2){
+    return n1+n2;
+}
+function subtracao(n1,n2){
+    return n1-n2;
+}
+function multiplicacao(n1,n2){
+    return n1*n2;
+}
+function divisao(n1,n2){
+    return n1/n2;
+}
+
+    let n1 = document.getElementById("n1")
+    let n2 = document.getElementById("n2")
+   function somar(){
+     document.getElementById("resultado").value = soma(Number(n1.value), Number(n2.value))
+}
