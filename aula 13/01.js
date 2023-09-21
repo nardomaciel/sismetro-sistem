e373c9f190d3293f103112d0640c839a3550c48f
nardo <129193276+nardomaciel@users.noreply.github.com>
2023-09-21 14:49:15 -0300

    script

--- aula 13/01.js
+++ aula 13/01.js
@@ -16,3 +16,12 @@ function divisao(n1,n2){
    function somar(){
      document.getElementById("resultado").value = soma(Number(n1.value), Number(n2.value))
 }
+function subtrair(){
+    document.getElementById("resultado").value = subtracao(Number(n1.value), Number(n2.value))
+}
+function multiplicar(){
+    document.getElementById("resultado").value = multiplicacao(Number(n1.value), Number(n2.value))
+}
+function dividir(){
+    document.getElementById("resultado").value = soma(Number(n1.value), Number(n2.value))
+}
