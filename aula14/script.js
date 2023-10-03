e57970cc3a0984e0fe7c5c6bf0629b5d1e59c293
nardo <129193276+nardomaciel@users.noreply.github.com>
2023-10-03 15:59:14 -0300

    mrs

--- aula14/script.js
+++ aula14/script.js
@@ -1,6 +1,7 @@
 var valor;
 var operacao;
 var num;
+var memoria = 0
 
 function efet_soma() {
     if (typeof valor != typeof undefined && typeof operacao != typeof undefined) {
@@ -56,19 +57,39 @@ function efet_porcent() {
 }
 // aaaaaaaaaaaaaaaaaaaaaaaaa
 
+function add_num() {
+    valor = mais_num(Number(document.getElementById("valor").value))
+}
 
-
-
+function subt_num() {
+    valor = men_num(Number(document.getElementById("valor").value))
+}
+function apag_memoria(){
+    memoria = 0
+    return console.log(memoria)
+}
+function most_memo(){
+    document.getElementById("valor").value = memoria
+    
+}
 function resultado() {
     document.getElementById("valor").value = operacao(valor, Number(document.getElementById("valor").value))
     valor = undefined
     operacao = undefined
+}
+function add_ponto(){
+    if(!document.getElementById("valor").value.includes(".")){
+        document.getElementById("valor").value = document.getElementById("valor").value.concat(".");
+    }
 }
 function inserirNum(num) {
     document.getElementById("valor").value = document.getElementById("valor").value.concat(num);
 }
 function exc_num() {
     document.getElementById("valor").value = document.getElementById("valor").value.substring(0, document.getElementById("valor").value.length - 1)
+}
+function apagar() {
+    document.getElementById("valor").value = document.getElementById("valor").value.substring(0, document.getElementById("valor").value.length - document.getElementById("valor").value.length) 
 }
 
 
@@ -92,6 +113,15 @@ function porcent(n1, n2) {
         return (n2 / 100) * n1
     }
 }
+function mais_num(n1){
+    memoria += n1
+    return console.log(memoria)
+}
+function men_num(n1){
+    memoria -= n1
+    return console.log(memoria)
+}
+
 
 let n1 = document.getElementById("n1")
 let n2 = document.getElementById("n2")
